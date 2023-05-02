fd4ad620fd31562d1c0ac3a0b11d002df74eeb52
( Nechiforel David-Samuel ) NsdHSO <nechiforelsamuel@yahoo.com>
2023-05-02 17:49:20 +0300

    chore:🌻 added corect name

---
+++ src/components/Friends.vue
@@ -0,0 +1,20 @@
+<script
+    setup
+    lang="ts"
+>
+
+const props = defineProps<{msg: string}>()
+
+</script>
+
+<template>
+  <div>
+    {{msg}}
+  </div>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
