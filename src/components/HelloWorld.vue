2fe92ce007ea4f68362233dbe6976f2f98bf7ee1
( Nechiforel David-Samuel ) NsdHSO <nechiforelsamuel@yahoo.com>
2023-05-02 09:28:41 +0300

    chore:🌻 added setup

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,7 +1,10 @@
-<script setup lang="ts">
-import { ref } from 'vue'
+<script
+    setup
+    lang="ts"
+>
+import { ref } from 'vue';
 
-defineProps<{ msg: string }>()
+defineProps<{msg: string}>()
 
 const count = ref(0)
 </script>
@@ -10,7 +13,11 @@ const count = ref(0)
   <h1>{{ msg }}</h1>
 
   <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
+    <button
+        type="button"
+        @click="count++"
+    >count is {{ count }}
+    </button>
     <p>
       Edit
       <code>components/HelloWorld.vue</code> to test HMR
@@ -19,13 +26,18 @@ const count = ref(0)
 
   <p>
     Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
+    <a
+        href="https://vuejs.org/guide/quick-start.html#local"
+        target="_blank"
+    >create-vue</a
     >, the official Vue + Vite starter
   </p>
   <p>
     Install
-    <a href="https://github.com/vuejs/language-tools" target="_blank">Volar</a>
+    <a
+        href="https://github.com/vuejs/language-tools"
+        target="_blank"
+    >Volar</a>
     in your IDE for a better DX
   </p>
   <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
